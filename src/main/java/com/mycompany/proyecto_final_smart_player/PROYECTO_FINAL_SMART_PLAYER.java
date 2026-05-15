3e683e7638aa70afd867fc03458a12c7a166bc2c
grija <grija@SOMBRA>
2026-05-14 22:02:31 -0600

    1

---
+++ src/main/java/com/mycompany/proyecto_final_smart_player/PROYECTO_FINAL_SMART_PLAYER.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_smart_player;
+
+/**
+ *
+ * @author grija
+ */
+public class PROYECTO_FINAL_SMART_PLAYER {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
